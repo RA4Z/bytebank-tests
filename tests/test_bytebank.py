12c34d14afcdf72fdfb3b89b63cd9b75640c203b
RA4Z <rraz639@gmail.com>
2023-07-14 17:08:45 -0300

    Implementando decrescimo do salario com TDD

--- tests/test_bytebank.py
+++ tests/test_bytebank.py
@@ -23,3 +23,14 @@ class TestClass:
         resultado = robert.sobrenome()
 
         assert resultado == esperado
+
+    def test_quando_decrescimo_salario_recebe_100000_deve_retornar_90000(self):
+        entrada_salario = 100000
+        entrada_nome = 'Robert Zimmermann'
+        esperado = 90000
+
+        funcionario_teste = Funcionario(entrada_nome, '31/07/2003', entrada_salario)
+        funcionario_teste.decrescimo_salario()
+        resultado = funcionario_teste.salario
+
+        assert resultado == esperado
